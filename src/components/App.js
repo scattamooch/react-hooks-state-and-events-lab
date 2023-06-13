e97e8452fc2e20f408dcb54a1f1ef20acd5f047c
scattamooch <seanstevens729@gmail.com>
2023-06-13 16:20:09 -0400

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
 
 function App() {
+  const [background, setBackground] = useState(false);
+    console.log(background);
+  // 1) Declare state variable and function = useState(condition) --> 2) Add event listener to appropraite button, set to invoke appropriate function --> 3) write function to change useState 
+  // 4) make a ternary or some other conditional for comparison/execution
+  const appClass = background ? "App dark" : "App light"
 
-  // replace 'false' with a state variable that can be toggled between true and false
-  // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
-
+  function handleClick() {
+    setBackground(!background);
+  }
   return (
     <div className={appClass}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button onClick={handleClick}>Dark Mode</button>
       </header>
       <ShoppingList items={itemData} />
     </div>
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+
+const [addItem, setItemAdded] = useState(false);
+
+function handleClick() {
+  setItemAdded(!addItem);
+}
+
+const itemClass = addItem ? "in-cart" : "";
+const buttonText = addItem ? "Remove from Cart" : "Add to Cart";
   return (
-    <li className="">
+    <li className={itemClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleClick}>{buttonText}</button>
     </li>
   );
 }
